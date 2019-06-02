c849b103b1e07698ff4b42b483353fdbaf311fb0
Infinish <Arich212@gmail.com>
2019-06-01 21:21:21 -0500

    fixed wilks state

--- src/reducers/getUserReducer.js
+++ src/reducers/getUserReducer.js
@@ -2,10 +2,8 @@ export default (state={}, action={}) => {
     switch (action.type) {
         case 'GET_USER':
             return {...state, ...action.payload};
-        case 'GET_WILKS': 
-            return{...state, ...action.payload};
         case 'SIGN_OUT': 
-            return {...state }
+            return {state: {}}
         default:
             return state
     }
